40993b5754c91892b648b052e2b067dbbfc9edea
talha siddiuque <talhasiddiquemeo007@gmail.com>
2024-02-26 18:16:11 +0500

    update

--- index.dart
+++ index.dart
@@ -1,10 +1,35 @@
-import "auth.dart";
-import "car.dart";
-import 'dart:convert';
-void main() {
-  print('Welcome To MEO Rent a Car');
-  print('');
-  Auth();
-  Car();
+import 'dart:io';
+import 'dart:math';
+import 'auth.dart';
 
+import 'data.dart';
+
+void main() {
+
+  var auth = Auth();
+  var data = Data();
+  Random random = Random();
+
+  int randomInt = random.nextInt(10000);
+  print('welcome');
+  print('');
+
+  data.cars.asMap().forEach((index, element) {
+    print('');
+    print('$index for ${element['name']}');
+  });
+  print("Select Category");
+  var carOptions = stdin.readLineSync();
+  var carTypes = data.cars[int.parse(carOptions!)];
+  carTypes['cars'].asMap().forEach((index, element) {
+    print('');
+    print('$index for ${element}');
+  });
+  print("Select car ");
+  var option = stdin.readLineSync();
+
+  var car = carTypes['cars'][int.parse(option!)];
+
+  print('You Select ${data.cars[int.parse(carOptions)]['name']}-${car}');
+  print('price $randomInt \$');
 }
